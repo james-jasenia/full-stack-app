bb41e1adae5cfbb4a9f8e8fc1f0cd5be944ab4ce
James <james.jasenia@gmail.com>
2023-11-02 22:08:56 +1100

    Reorganize repository into full-stack-app structure

---
+++ user-postgres-datastore/seed-scripts/01-create-tables.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users (
+  id serial PRIMARY KEY,
+  username VARCHAR (50) UNIQUE NOT NULL,
+  firstName VARCHAR (50) UNIQUE NOT NULL,
+  lastName VARCHAR (50) UNIQUE NOT NULL,
+  email VARCHAR (100) UNIQUE NOT NULL
+);
---
+++ user-postgres-datastore/seed-scripts/02-insert-data.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (username, firstName, lastName email) VALUES
+  ('user1', 'John', 'Doe', 'user1@example.com'),
+  ('user2', 'Jane', 'Doe', 'user2@example.com');
